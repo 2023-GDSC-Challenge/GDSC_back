d222abbd8389d7a82379f17676bf758cd64a16e4
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-15 17:54:47 +0900

    feat: Member CRUD finish

--- src/main/java/com/solution/green/controller/MemberController.java
+++ src/main/java/com/solution/green/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @Slf4j // log
@@ -29,37 +30,25 @@ public class MemberController {
         return userService.getAllMembers();
     }
 
+    @GetMapping("/members/{memberId}")
+    public CreateMember.Response getMemberDetail(
+            @PathVariable final String memberId
+    ) throws Exception {
+        return userService.getMemberDetail(memberId);
+    }
 
-//    @GetMapping("/developers")
-//    public List<DeveloperDto> getAllDevelopers() {
-//        log.info("GET /developers HTTP/1.1");
-//        return dMakerService.getAllEmployedDevelopers();
-//    }
-//
-//    @GetMapping("/developers/{memberId}")
-//    public DeveloperDetailDto getDeveloperDetail(
-//            @PathVariable final String memberId // 입력받은 값은 바뀌지 않음 - final
-//    ) {
-//        log.info("GET /developers HTTP/1.1");
-//
-//        return dMakerService.getDeveloperDetail(memberId);
-//    }
-//
-//
-//    @PutMapping("/developers/{memberId}")
-//    public DeveloperDetailDto editDeveloper(
-//            @PathVariable final String memberId, // for [Data binding]
-//            @Valid @RequestBody EditDeveloper.Request request
-//    ) {
-//        log.info("GET /developers HTTP/1.1");
-//
-//        return dMakerService.editDeveloper(memberId, request);
-//    }
-//
-//    @DeleteMapping()
-//    public DeveloperDetailDto deleteDeveloper(
-//            @PathVariable final String memberId
-//    ){
-//        return dMakerService.deleteDeveloper(memberId);
-//    }
+    @PutMapping("/members/{memberId}")
+    public String editMember(
+            @PathVariable final String memberId,
+            @Valid @RequestBody CreateMember.Request request
+    ) throws Exception {
+        return userService.editMember(memberId, request);
+    }
+
+    @DeleteMapping("/members/{memberId}")
+    public void deleteMember(
+            @PathVariable final String memberId
+    ){
+        log.info(userService.deleteMember(memberId));
+    }
 }
--- src/main/java/com/solution/green/service/UserService.java
+++ src/main/java/com/solution/green/service/UserService.java
@@ -10,134 +10,67 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 @Service
 public class UserService {
     public static final String COLLECTION_NAME = "users"; // database name
+    // TODO database name -> code
     public static Firestore firestore = FirestoreClient.getFirestore();
 
     public String createMember(CreateMember.Request request) throws Exception{
-//        validateCreateMemberRequest(request); // TODO
-        ApiFuture<WriteResult> apiFuture =
-                firestore.collection(COLLECTION_NAME)
-                        .document(request.getMemberId().toString()).set(request);
+        DocumentReference documentReference =
+                validateCreateMemberRequest(request.getMemberId());
+        ApiFuture<WriteResult> apiFuture = documentReference.set(request);
         return apiFuture.get().getUpdateTime().toString();
     }
-    private void validateCreateMemberRequest(@NonNull CreateMember.Request request) {
-//        developerRepository.findByMemberId(request.getMemberId())
-//                .ifPresent((developer -> {
-//                    throw new DMakerException(DUPLICATED_MEMBER_ID);
-//                }));
-
-//        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
-//        DocumentReference docIdRef = rootRef.collection("yourCollection").document(docId);
-//        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    DocumentSnapshot document = task.getResult();
-//                    if (document.exists()) {
-//                        Log.d(TAG, "Document exists!");
-//                    } else {
-//                        Log.d(TAG, "Document does not exist!");
-//                    }
-//                } else {
-//                    Log.d(TAG, "Failed with: ", task.getException());
-//                }
-//            }
-//        });
+    private DocumentReference validateCreateMemberRequest(@NonNull String memberId) throws Exception {
+        DocumentReference documentReference =
+                firestore.collection(COLLECTION_NAME).document(memberId);
+        DocumentSnapshot documentSnapshot = documentReference.get().get();
+        if (!documentSnapshot.exists())
+            throw new Exception(); // TODO implement - duplicate memberId
+        else
+            return documentReference;
     }
 
     public List<CreateMember.Response> getAllMembers() throws Exception{
         List<CreateMember.Response> responseList = new ArrayList<>();
 
-        ApiFuture<QuerySnapshot> future =
-                firestore.collection(COLLECTION_NAME).get();
-        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-        for (QueryDocumentSnapshot document : documents) {
-            responseList.add(
-                    document.toObject(CreateMember.Response.class));
-        }
+        List<QueryDocumentSnapshot> documents =
+                firestore.collection(COLLECTION_NAME)
+                        .get()
+                        .get()
+                        .getDocuments();
+        for (QueryDocumentSnapshot document : documents)
+            responseList.add(document.toObject(CreateMember.Response.class));
 
         return responseList;
     }
-    public CreateMember getEmployeeDetail() throws Exception{
+    public CreateMember.Response getMemberDetail(String memberId) throws Exception{
+        DocumentSnapshot documentSnapshot = firestore.collection(COLLECTION_NAME)
+                .document(memberId)
+                .get()
+                .get();
+        if(documentSnapshot.exists())
+            return documentSnapshot.toObject(CreateMember.Response.class);
+        else
+            return null; // TODO error handling
+    }
+    public String editMember(String memberId, CreateMember.Request request)
+            throws Exception {
         DocumentReference documentReference =
-                firestore.collection(COLLECTION_NAME).document("1");
-        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
-        DocumentSnapshot documentSnapshot = apiFuture.get();
-        CreateMember createMember = null;
-        if(documentSnapshot.exists()){
-            createMember = documentSnapshot.toObject(CreateMember.class);
-            return createMember;
-        }
-        else{
-            return null;
-        }
+                validateCreateMemberRequest(request.getMemberId());
+        // TODO 어떤 내용을 editable 하게 설정할 건지 추후 논의 (반드시 id는 변경 불가하도록)
+        ApiFuture<WriteResult> apiFuture = documentReference.set(request);
+        return apiFuture.get().getUpdateTime().toString();
     }
 
-//    @Transactional(readOnly = true) // 내부에서 데이터가 바뀌지 않도록 할 수 있음
-//    public List<DeveloperDto> getAllEmployedDevelopers() {
-//        return developerRepository.findDevelopersByStatusCodeEquals(StatusCode.EMPLOYED)
-//                .stream() // 컬렉션의 요소를 하나씩 참조, 람다식으로 처리하는 반복자
-//                .map(DeveloperDto::fromEntity) // entity -> dto
-//                .collect(Collectors.toList()); // 리스트로
-//    }
-//    @Transactional(readOnly = true)
-//    public DeveloperDetailDto getDeveloperDetail(String memberId) {
-//        return DeveloperDetailDto.fromEntity(getDeveloperByMemberId(memberId));
-//    }
-//    private Developer getDeveloperByMemberId(String memberId){
-//        // naming convention ** get~ : 반드시 ~가 존재해야함
-//        return developerRepository.findByMemberId(memberId)
-//                .orElseThrow( () -> new DMakerException(NO_DEVELOPER));
-//    }
-//    @Transactional(readOnly = true)
-//    public DeveloperDetailDto editDeveloper(String memberId, EditDeveloper.Request request) {
-//
-//        request.getDeveloperLevel().validateExperienceYears(
-//                request.getExperienceYears());
-//        // public 메소드에는 큼직한 내용을 담고
-//        // private 메소드에 상세 사항을 구현할 것
-//
-//        // 수정할 때는, entity 로 받아와서 set 으로 수정
-//        // @Transactional 가 Dirty Checking 을 동작,
-//        // 알아서 DB에 반영해주기 때문에
-//        // 별도로 save 는 필요없다.
-//        // ** save 는 create, delete 에서만 사용
-//        return DeveloperDetailDto.fromEntity(
-//                getUpdatedDeveloperFromRequest(request, getDeveloperByMemberId(memberId)));
-//    }
-//
-//    private Developer getUpdatedDeveloperFromRequest(EditDeveloper.Request request, Developer developer) {
-//        developer.setDeveloperLevel(request.getDeveloperLevel());
-//        developer.setDeveloperSkillType(request.getDeveloperSkillType());
-//        developer.setExperienceYears(request.getExperienceYears());
-//
-//        return developer;
-//    }
-//
-//    private static void validateDeveloperLevel(
-//            DeveloperLevel developerLevel, Integer experienceYears) {
-//        developerLevel.validateExperienceYears(experienceYears);
-//    }
-//
-//    @Transactional
-//    public DeveloperDetailDto deleteDeveloper(String memberId) {
-//        // 1. EMPLOYED -> RETIRED
-//        Developer developer = developerRepository.findByMemberId(memberId)
-//                .orElseThrow(() -> new DMakerException(NO_DEVELOPER));
-//        developer.setStatusCode(StatusCode.RETIRED);
-//
-//        if (developer != null) throw new DMakerException(NO_DEVELOPER);
-//
-//        // 2. save into RetiredDeveloper
-//        RetiredDeveloper retiredDeveloper = RetiredDeveloper.builder()
-//                .memberId(memberId)
-//                .name(developer.getName())
-//                .build();
-//        retiredDeveloperRepository.save(retiredDeveloper);
-//        return DeveloperDetailDto.fromEntity(developer);
-//    }
+    public String deleteMember(String memberId) {
+        ApiFuture<WriteResult> apiFuture =
+                firestore.collection(COLLECTION_NAME).document(memberId)
+                        .delete();
+        return "Document id: " + memberId + " delete";
+    }
 }

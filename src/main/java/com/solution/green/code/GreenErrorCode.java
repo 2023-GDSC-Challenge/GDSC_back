389a1bda8a88f385d673885703ea29cee7908141
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-23 18:33:07 +0900

    feat: add error code

--- src/main/java/com/solution/green/code/GreenErrorCode.java
+++ src/main/java/com/solution/green/code/GreenErrorCode.java
@@ -6,27 +6,37 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum GreenErrorCode {
+    ALREADY_EXISTED(500, "CREATE-PERSON-ERR-500","have already been invited."),
+    ALREADY_REGISTERED(500, "CREATE-USER-ERR-500","is already registered."),
 
-    NO_MEMBER("There is no corresponding MEMBER."),
-    WRONG_PASSWORD("Invalid password."),
-    ALREADY_REGISTERED("This member is already registered."),
+    WRONG_PASSWORD(500, "LOGIN-ERR-500","Invalid password."),
+    WRONG_EMAIL(500,"LOGIN-ERR-500","There is no such email information."),
 
-    INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
-    INVALID_REQUEST("잘못된 요청입니다.")
+    INVALID_DELETE_SUPERUSER(500, "INVALID-ERR-500", "SuperUser cannot be deleted from Travel."),
+    INVALID_DELETE_NOTSUPERUSER(500, "INVALID-ERR-500", "Only SuperUser can delete the Travel."),
+    INVALID_DELETE_EVENT_EXISTED(500, "INVALID-ERR-500", "This Person has participated Events."),
+    INVALID_DELETE_TRAVEL_EXISTED(500, "INVALID-ERR-500",
+            "This Person has participated Travel. \n" +
+                    "Please withdraw from the trip to deregister."),
 
-    // TODO - not yet
-//    CREATE_FAIL("Failed to create entity on request."),
-//    CREATE_EVENT_FAIL(500, "CREATE-ERR-500", "Failed to create event entity on request."),
-//    CREATE_PARTICIPANT_FAIL(500, "CREATE-ERR-500", "Failed to create participant entity on request."),
-//
-//    DELETE_FAIL(500, "DELETE-ERR-500", "Failed to delete entity on request."),
-//
-//    INVALID_DELETE_SUPERUSER(500, "INVALID-ERR-500", "SuperUser cannot be deleted from Travel."),
-//    INVALID_DELETE_NOTSUPERUSER(500, "INVALID-ERR-500", "Only SuperUser can delete the Travel."),
-//    INVALID_DELETE_EVENTEXISTED(500, "INVALID-ERR-500", "This Person has participated Events."),
-//
-//    INVALID_DELETE_TRAVELEXISTED(500, "INVALID-ERR-500", "This Person has participated Travel.")
+    NO_MEMBER(500, "MEMBER-ERR-500", "There is no corresponding MEMBER."),
+    NO_PERSON(500, "PERSON-ERR-500", "There is no corresponding PERSON."),
+    NO_PARTI(500, "PARTI-ERR-500", "There is no corresponding PARTICIPANT."),
+    NO_MANAGER(500, "EVENT-ERR-500","There is no MANAGER for the event."),
+    NO_SUPERUSER(500, "TRAVEL-SUPERUSER-ERROR", "There is no SuperUser on this trip."),
+    NO_TRAVEL(500, "TRAVEL-ERR-500","There is no corresponding TRAVEL."),
+    NO_EVENT(500, "EVENT-ERR-500","There is no corresponding EVENT."),
+
+    CREATE_FAIL(500, "CREATE-ERR-500", "Failed to create entity on request."),
+
+    NOT_FOUND(404,"COMMON-ERR-404","PAGE NOT FOUND"),
+    INTER_SERVER_ERROR(500,"COMMON-ERR-500","INTER SERVER ERROR"),
+
+
+
+
     ;
-
+    private int status;
+    private String errorCode;
     private String message;
 }

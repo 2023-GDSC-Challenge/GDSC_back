ed7a42cbb00081fbbbe87d93b43fa9020bf705c2
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-24 11:18:04 +0900

    feat: create image method

--- src/main/java/com/solution/green/controller/MemberController.java
+++ src/main/java/com/solution/green/controller/MemberController.java
@@ -1,19 +1,29 @@
 package com.solution.green.controller;
 
 import com.solution.green.dto.MemberDto;
+import com.solution.green.service.GCSService;
 import com.solution.green.service.MemberService;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class MemberController {
+    @GetMapping("")
+    public String home() {
+        return "This is index page!\n" +
+                "다른 페이지로 접속하기 위해서는 http://35.216.34.93:8080/api 뒷부분에\n" +
+                "notion api 페이지에 있는 endpoint를 붙여주세요!";
+    }
     @GetMapping("/getTest")
     public String getTest() {
         return "Get Mapping Test Success!";
@@ -24,8 +34,8 @@ public class MemberController {
     }
     /*-----------------------front connecting test-----------------------*/
 
-    @Autowired
-    MemberService memberService;
+    private final MemberService memberService;
+    private final GCSService gcsService;
 
     @PostMapping("/create-member")
     public MemberDto.Response createMember(
@@ -33,24 +43,38 @@ public class MemberController {
         return memberService.createMember(request);
     }
 
-    @GetMapping("/members")
+    @GetMapping("/get-all-members")
     public List<MemberDto.Response> getAllMembers() {
         return memberService.getAllMembers();
     }
 
-    @GetMapping("/members/{memberId}")
+    @GetMapping("/get-member-detail/{memberId}")
     public MemberDto.Response getMemberDetail(@PathVariable final Long memberId) {
         return memberService.getMemberDetail(memberId);
     }
 
-    @PutMapping("/members/{memberId}")
-    public MemberDto.Response editMember(
-            @PathVariable final Long memberId,
-            @Valid @RequestBody MemberDto.Request request) {
-        return memberService.editMember(memberId, request);
+    @GetMapping("/getUserImage/{memberId}")
+    public String getUserImage(@PathVariable final Long userId) {
+        return memberService.getUserImageURL(userId);
     }
 
-    @DeleteMapping("/members/{memberId}")
+    @PatchMapping("/update-member-image/{memberId}")
+    public String updateMemberImage(@PathVariable final Long userId,
+                                    @RequestPart(value="file") MultipartFile file)
+            throws IOException {
+        String uuid = gcsService.uploadImage(file);
+        memberService.updateMemberImage(userId, gcsService.uploadImage(file));
+        return uuid;
+    }
+
+    @PutMapping("/update-member/{memberId}")
+    public MemberDto.Response updateMember(
+            @PathVariable final Long memberId,
+            @Valid @RequestBody MemberDto.Request request) {
+        return memberService.updateMember(memberId, request);
+    }
+
+    @DeleteMapping("/delete-member/{memberId}")
     public void deleteMember(@PathVariable final Long memberId) {
         memberService.deleteMember(memberId);
     }

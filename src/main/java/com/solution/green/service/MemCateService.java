12a920ccd0c369e0d5512830d40a1f9fda7e3ee4
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-27 19:05:49 +0900

    feat: move getQuestNumPerCategory
    MemDoService -> MemCateService

--- src/main/java/com/solution/green/service/MemCateService.java
+++ src/main/java/com/solution/green/service/MemCateService.java
@@ -3,7 +3,10 @@ package com.solution.green.service;
 import com.solution.green.dto.CategoryDto;
 import com.solution.green.dto.MemCateDto;
 import com.solution.green.entity.MemberCategory;
+import com.solution.green.exception.GreenException;
+import com.solution.green.repository.CategoryRepository;
 import com.solution.green.repository.MemCateRepository;
+import com.solution.green.repository.MemDoRepository;
 import com.solution.green.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,17 +16,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.solution.green.code.GreenErrorCode.NO_CATEGORY;
+
 @Service
 @RequiredArgsConstructor
 public class MemCateService {
     private final MemCateRepository memCateRepository;
-
+    private final MemDoRepository memDoRepository;
+    private final CategoryRepository categoryRepository;
     // TODO - 멤버가 카테고리에 대해 어떻게 평가했는지 get
+
     @Transactional
     public void getMemberCategory(Long memberId) {
         List<CategoryDto.Home> list = getCateHomeDtoList(memberId);
-    }
+        for(CategoryDto.Home cate : list) {
 
+        }
+    }
+    private Double setAchieveRate(Long categoryId) {
+        return null;
+    }
+    @Transactional(readOnly = true)
+    public Long getQuestNumPerCategory(Long categoryId) {
+        return memDoRepository.countByQuest_SubCategory_Category(
+                categoryRepository.findById(categoryId)
+                        .orElseThrow(() -> new GreenException(NO_CATEGORY))
+        );
+    }
     @Transactional(readOnly = true)
     private List<CategoryDto.Home> getCateHomeDtoList(Long memberId){
         return memCateRepository.findByMember_IdOrderByPriorityAsc(memberId)
--- src/main/java/com/solution/green/service/MemDoService.java
+++ src/main/java/com/solution/green/service/MemDoService.java
@@ -1,24 +1,10 @@
 package com.solution.green.service;
 
-import com.solution.green.exception.GreenException;
-import com.solution.green.repository.CategoryRepository;
-import com.solution.green.repository.MemDoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import static com.solution.green.code.GreenErrorCode.NO_CATEGORY;
 
 @Service
 @RequiredArgsConstructor
 public class MemDoService {
-    private final MemDoRepository memDoRepository;
-    private final CategoryRepository categoryRepository;
-    @Transactional(readOnly = true)
-    public Long getQuestNumPerCategory(Long categoryId) {
-        return memDoRepository.countByQuest_SubCategory_Category(
-                categoryRepository.findById(categoryId)
-                        .orElseThrow(() -> new GreenException(NO_CATEGORY))
-        );
-    }
+
 }

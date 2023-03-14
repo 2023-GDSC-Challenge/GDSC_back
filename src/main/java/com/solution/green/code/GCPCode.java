7bfa324c63a02df99fe1bf72556945881f9eaaaf
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-03-14 19:16:29 +0900

    hotfix: image upload
    json 파일을 확인하지 못하는 에러 해결
    FileInputStream -> ClassPathResource.getInputStream()

--- src/main/java/com/solution/green/code/GCPCode.java
+++ src/main/java/com/solution/green/code/GCPCode.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum GCPCode {
-    SERVICE_ACCOUNT_JSON_PATH("C:/Users/12nov/바탕 화면/CodeAfter22F/green/src/main/resources/gcpBucketAccessKey.json"),
+    SERVICE_ACCOUNT_JSON_PATH("gcpBucketAccessKey.json"),
     // TODO - 이거 설정 안하면 com.google.api.client.googleapis.json.GoogleJsonResponseException: 401 Unauthorized POST https://storage.googleapis.com/upload/storage/ 에러 발생
     PROJECTED("gothic-imprint-378407"),
     ;
